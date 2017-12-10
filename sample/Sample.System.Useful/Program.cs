ee88f28b866047a2c05064a4693c2b88c5801049
atesz77 <34383667+atesz77@users.noreply.github.com>
2017-12-10 09:59:30 +0100

    add sample

---
+++ sample/Sample.System.Useful/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sample.System.Useful
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello System.Useful!");
+
+            Console.WriteLine('a'.GetNumber()); //1
+            Console.WriteLine('a'.GetNumberOffset()); //10
+
+            Console.WriteLine('a'.GetNumber(9)); //10
+            Console.WriteLine('a'.GetNumberOffset()); //10
+
+            Console.WriteLine('A'.GetNumber()); //1
+            Console.WriteLine('A'.GetNumberOffset()); //10
+
+            Console.WriteLine('A'.GetNumber(1)); //2
+            Console.WriteLine('B'.GetNumber()); //2
+        }
+    }
+}
